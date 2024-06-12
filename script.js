5076a52dc116e3ba303c5090d0cc3df767cf2677
MantisToboggan2 <felid567@hotmail.com>
2024-06-12 11:47:26 -0400

    Deleted home.html file and fixed css, html code

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("testButton").addEventListener("click", function() {
         let currentTime = new Date().toLocaleTimeString();
         document.getElementById("timeDisplay").innerText = "Current Time: " + currentTime;
+        timeDisplay.classList.add('styled-theme');
     });
 });
